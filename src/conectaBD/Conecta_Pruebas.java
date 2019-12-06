fbbb305a24e2ff5d93861f75df82f1c0de2c2c55
Gerard B <barbosatrix@hotmail.com>
2019-12-05 19:30:00 -0500

    Formulario de registro en base de datos mysql con conector, statement y execute

---
+++ src/conectaBD/Conecta_Pruebas.java
@@ -0,0 +1,20 @@
+package conectaBD;
+
+public class Conecta_Pruebas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		try {
+			
+	
+			
+		}catch(Exception e) {
+			
+		}
+		
+		
+		
+	}
+
+}
